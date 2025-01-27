a592c3be419851528e1694d8c6b5ade422b31a28
matheusrmello <matheus.rm.09@hotmail.com>
2025-01-26 22:24:15 -0300

    feat: remote backend

--- provider.tf
+++ provider.tf
@@ -1,9 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.84.0"
     }
+  }
+  backend "s3" {
+    bucket = "user-test-tfstate-eks"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
