a5c427750731077264264c899e3612210ef141a1
Matheus Mello <50120961+matheusrmello@users.noreply.github.com>
2025-01-30 18:08:18 -0300

    fix: adding output to module

--- output.tf
+++ output.tf
@@ -2,10 +2,14 @@
 #   value = module.eks_cluster.oidc
 # }
 
-# output "ca" {
-#   value = module.eks_cluster.certificate_authority
-# }
+output "ca" {
+  value = module.eks_cluster.certificate_authority
+}
 
-# output "endpoint" {
-#   value = module.eks_cluster.endpoint
-# }
+output "endpoint" {
+  value = module.eks_cluster.endpoint
+}
+
+output " cluster_name" {
+    value = module.eks_cluster.cluster_name
+}
